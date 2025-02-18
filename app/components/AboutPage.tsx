70deb314992f465b27cd490e061c3fcb864ea809
fasuyaaaPNG <dhavinf.a@gmail.com>
2025-02-19 00:28:07 +0700

    new

--- app/components/AboutPage.tsx
+++ app/components/AboutPage.tsx
@@ -12,7 +12,7 @@ export default function AboutPage() {
     <div className="border bg-[#DAD5DF] h-[289px] xl:h-[342px] rounded-[12px] flex flex-row">
       <div id="boxKiri" className="bg-[url('/Profile/profile.png')] bg-cover rounded-[12px] border-4 border-[#0C2937] w-[289px] h-[289px] xl:w-[340px] xl:h-[340px]"></div>
       <div className="secKanan xl:w-[860px] w-[632px] pt-[30px] xl:pt-[40px] pb-[40px]">
-        <div className={`${chivoMono.className} flex items-center gap-[27px] xl:gap-[50px] w-max ml-[60px] mb-[29px]`}>
+        <div className={`${chivoMono.className} flex items-center gap-[27px] xl:gap-[50px] w-max ml-[30px] mb-[29px]`}>
             <p className='font-extrabold text-[16px] xl:text-[18px] text-[#624D74]'>
                 DHAVIN FASYA ALVIYANTO
             </p>
@@ -29,8 +29,8 @@ export default function AboutPage() {
             </div>
         </div>
         <hr className='w-full bg-[#624D74] border-[#624D74] mb-[23px] xl:mb-[33px] h-1'/>
-        <div className={`${chivoMono.className} flex flex-col justify-between ml-[60px] w-[540px] xl:w-[780px]`}>
-            <p className='text-[12px] xl:text-[14px] mb-[50px] xl:mb-[80px]'>
+        <div className={`${chivoMono.className} flex flex-col justify-between ml-[30px] w-[540px] xl:w-[780px]`}>
+            <p className='text-[12px] xl:text-[14px] mb-[50px] xl:mb-[80px] text-justify'>
                 Hey, im using WhatsApp!:D
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Id consectetur et animi soluta sed. Est necessitatibus repellat provident, asperiores ea similique pariatur fuga aperiam assumenda eveniet officia saepe inventore! Perspiciatis illo veritatis maxime nisi magnam non odio temporibus.
             </p>
